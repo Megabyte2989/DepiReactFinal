b655c92c8c8996b2f4898fcda886774d4db5f172
kerlous0 <kerlousnasser0@gmail.com>
2024-10-06 15:53:14 +0300

    skip brute force attack

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -43,13 +43,13 @@ router.post("/login", async (req, res) => {
     // Find the user by email
     const user = await User.findOne({ email });
     if (!user) {
-      return res.status(400).json({ message: "Wrong Email" });
+      return res.status(400).json({ message: "Wrong Email or Password" });
     }
 
     // Compare the password
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
-      return res.status(400).json({ message: "Wrong Password" });
+      return res.status(400).json({ message: "Wrong Email or Password" });
     }
 
     // Generate a JWT
