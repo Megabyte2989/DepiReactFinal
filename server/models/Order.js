fd137765f354f7a8508885f10bc8035f40c220d6
Blaxinoss <abdulllahismael078@gmail.com>
2024-10-23 05:47:50 +0300

    Booking roll back

--- server/models/Order.js
+++ server/models/Order.js
@@ -10,30 +10,6 @@ const orderSchema = new mongoose.Schema({
         required: true,
         trim: true
     },
-    phoneNumber: {
-        type: String,
-        required: true,
-    },
-    pickupDate: {
-        type: Date,
-        required: true,
-    },
-    pickupTime: {
-        type: String,
-        required: true,
-    },
-    dropoffDate: {
-        type: Date,
-        required: true,
-    },
-    dropoffTime: {
-        type: String,
-        required: true,
-    },
-    location: {
-        type: String,
-        required: true,
-    },
     status: {
         type: String,
         enum: ['pending', 'accepted'],
@@ -48,14 +24,10 @@ const orderSchema = new mongoose.Schema({
         default: Date.now
     }
 });
-
-
 // Update the updatedAt field before saving the document
 orderSchema.pre('save', function (next) {
     this.updatedAt = Date.now();
     next();
 });
-
 const Order = mongoose.model('Order', orderSchema);
-
 module.exports = Order;
